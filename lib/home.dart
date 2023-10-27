66a52d25611a6da9639150f4fffae8df7482ad68
oussama_hatoum <ousama.hatoum@gmail.com>
2023-10-27 15:16:21 +0300

    Part 2 includes multiple TextFields and an ElevationButton.

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,13 +9,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   String _text = '';
+  double _x = -1, _y = -1;
 
-  void updateText(String text) {
+  void updateText() {
     setState(() {
-      _text = text;
+      if (_x == -1 || _y == -1) {
+        _text = 'Please fill all fields';
+      }
+      else {
+        _text = (_x + _y).toString();
+      }
     });
+  }
+
+  void updateX(String x) {
+    if (x.trim() == '') {
+      _x = -1;
+    }
+    else {
+      _x = double.parse(x);
+    }
+  }
+
+  void updateY(String y) {
+    if (y.trim() == '') {
+      _y = -1;
+    }
+    else {
+      _y = double.parse(y);
+    }
   }
 
   @override
@@ -28,20 +52,39 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               const SizedBox(height: 20.0),
-              Text('You typed: $_text', style: const TextStyle(fontSize: 18.0)),
-              const SizedBox(height: 20.0,),
-              SizedBox(width: 300.0, height: 50.0,
-                child: TextField(
-                  style: const TextStyle(fontSize: 18.0),
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: 'Enter some text'
-                  ),
-                  onChanged: (text) { updateText(text);},
-                ),
-              ),
+              Text('Sum: $_text', style: const TextStyle(fontSize: 18.0)),
+              const SizedBox(height: 20.0),
+              MyTextField(f: updateX, hint: 'Enter X'),
+              const SizedBox(height: 20.0),
+              MyTextField(f: updateY, hint: 'Enter Y'),
+              const SizedBox(height: 20.0),
+              ElevatedButton(onPressed: () {updateText();}, child: Text('SUM', style: TextStyle(fontSize: 24.0),))
             ],
           ),
         )
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+
+  Function(String) f;
+  String hint;
+
+  MyTextField({required this.hint, required this.f, super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: 300.0, height: 50.0,
+      child: TextField(
+        keyboardType: TextInputType.number,
+        inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[0-9.]'))],
+        style: const TextStyle(fontSize: 18.0),
+        decoration: InputDecoration(
+            border: const OutlineInputBorder(), hintText: hint
+        ),
+        onChanged: (text) { f(text);},
+      ),
     );
   }
 }
